68ab02d7f16423917e97a5cb03e67732d252ecac
Lamelo04 <salahibra04@gmail.com>
2024-05-02 23:28:09 +0100

    Initial commit

---
+++ src/Scripts/reviews.js
@@ -0,0 +1,27 @@
+export const customerReviews = [
+  {
+    "name": "Mohamed Ibrahim",
+    "location": "Sousse, Tunisia",
+    "message": "Health Plus transformed my healthcare experience! The online consultations were so convenient, and the doctors were knowledgeable and caring."
+  },
+  {
+    "name": "Rim Rezgui",
+    "location": "Sousse, Tunisia",
+    "message": "I found the perfect specialist for my condition through Health Plus. The personalized treatment plan made all the difference. Thank you for prioritizing my health!"
+  },
+  {
+    "name": "Mbarka Ben Fredj",
+    "location": "Mahdia, Tunisia",
+    "message": "Booking appointments was a breeze, and the service exceeded my expectations. The doctors truly listen and provide effective solutions. 5 stars!"
+  },
+  {
+    "name": "Wael Rezgui",
+    "location": "Sousse, Tunisia",
+    "message": "Health Plus is a game-changer! The emergency care saved me during a critical situation. Grateful for their quick and efficient support."
+  },
+  {
+    "name": "Med Ali Chebbi",
+    "location": "Sidi Bou Zid, Tunisia",
+    "message": "I used to dread dental visits, but Health Plus made it a pleasant experience. The dentist was gentle and professional. I'll definitely be back!"
+  }
+];
